5cac144eea76f215e72f55201d5df9f441a97e55
Kashif Malik <thailandvista7@gmai.com>
2026-01-06 04:01:29 +0700

    Disable Eslint during vercel build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     domains: ['res.cloudinary.com', 'via.placeholder.com'],
   },
+
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
